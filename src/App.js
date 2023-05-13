d7405d32fcdc39ee73cb13ec774f0948bce807af
ChahrazadBouragba <chbouragba25@gmail.com>
2023-05-13 12:38:13 +0100

    scores colors

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ let [count2, setCount2] = useState(0);
   return (
     <div className="App">
       <section class="team1">
-        {count1}
+        {count1 >= count2 ? <span style={{color: "green"}}>{count1}</span> : <span style={{color: "red"}}>{count1}</span>}
 
             <button onClick={() => setCount(count1 + 1)}>Increase</button>
             <button onClick={() => setCount(count1 - 1)}>Decrease</button>
@@ -18,8 +18,7 @@ let [count2, setCount2] = useState(0);
       </section>
 
       <section class="team2">
-        {count2}
-
+      {count2 < count1 ? <span style={{color: "red"}}>{count2}</span> : <span style={{color: "green"}}>{count2}</span>}
             <button onClick={() => setCount2(count2 + 1)}>Increase</button>
             <button onClick={() => setCount2(count2 - 1)}>Decrease</button>
             <button onClick={() => setCount2(count2 = 0)}>Set to Zero</button>
